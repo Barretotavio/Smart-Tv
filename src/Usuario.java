dfaead7589f6a669c0d25b780c1281d32c7a6334
Barretotavio <obarretomachado@gmail.com>
2023-03-13 16:36:56 -0300

    Insert new methods

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,8 +5,24 @@ public class Usuario {
         SmartTv smartTv = new SmartTv();
 
         System.out.println("A Tv está ligada? " + smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("Status -> Tv ligada? " + smartTv.ligada);
+
         System.out.println("Canal: " + smartTv.canal);
+
         System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("O volume está em: " + smartTv.volume);
+
+        smartTv.mudarCanal(15);
+
+        smartTv.desligar();
+        System.out.println("Status -> Tv ligada? " + smartTv.ligada);
 
     }
 
